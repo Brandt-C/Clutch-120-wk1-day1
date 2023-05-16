5f4b58da756ec956ba34f3003ce774958a31bf3b
Brandt Carlson <brandt.carlson11@gmail.com>
2023-05-15 19:42:35 -0500

    first commit

---
+++ fiveqs.py
@@ -0,0 +1,24 @@
+def hi(username):
+    print(f"I would like to introduce. . . : {username.title()}")
+
+# hi('Connor')
+
+# for i in range(5):
+#     print(i)
+
+# print(range(5))
+
+# print(list(range(5)))
+
+# def max_num_in_list(a_list):
+#    for list in a_list:
+#       list = int(list)
+#       list = max(list)
+#       print(list)
+
+# numbers = [1,2,3,4,5,6,7,8,9,10]
+# max_num_in_list(numbers)
+
+def max_of_list(a_list):
+   return max(a_list)
+print(max_of_list([1, 5, 9]))
